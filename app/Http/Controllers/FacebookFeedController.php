8dca3278d82308d7cadfc857c001e6c30e8435cd
Asaf53 <asafrushiti3@gmail.com>
2023-11-11 19:35:49 +0100

    fixes

--- app/Http/Controllers/FacebookFeedController.php
+++ app/Http/Controllers/FacebookFeedController.php
@@ -11,9 +11,9 @@ use Illuminate\Http\Request;
 
 class FacebookFeedController extends Controller
 {
-    
+
     //oauth/access_token?grant_type=fb_exchange_token&client_id=1269290357308216&client_secret=f6f3696ba4669168747fe3c21c1e66a4&fb_exchange_token=EAASCabDdOzgBANqvRZC1ygL31aZB8J4uaKC7vtthRbGT03d58WZC3ZA4n5KQfdWxUqiTRQDfEVtlgNBusvXBrbesiMRTdCiCgpSwH6AEE2YcwjE2SO2142RUoo0S6X0BjyTAz2ZAQZA5i7LkfVnkhQTA8ozogpNm2dXUzZBVtshSkJka4giivlrJ6QEDiR8LZBWjIavc1XsQQnf9gLZC7MjlH
-    
+
   public function scrapeFacebook()
   {
     $facebook = new Facebook([
@@ -22,7 +22,7 @@ class FacebookFeedController extends Controller
       'default_graph_version' => 'v16.0',
     ]);
 
-    $accessToken = 'EAASCabDdOzgBADPxV7dsZA7d2mo9024bB9UQZBDXowqBBnVNGU3wo06orFtljTZARNCRI8L6LBmxNVAFKMG9nB7fDxMIcJSMCY57q40uK3o3Rs7ZCjhR5LO5eWChozXlSb9PqirtAMXQ7VFaE98yVCeO3gAQaVV1qHpAEOG1CvZAuCYngQZAgV';
+    $accessToken = 'EAASCabDdOzgBO4jfyBO0uHgazwLzjHi1ZAcGq2b8cMj2qafNceZCX0gsLzN010EYNtgr0QrGEZAerKjXDZCBMQyN2GGvLGrXkI7WFEQAmxMdVPSlZAmRr6Ai3LVNsQ0U8iUHAJhhOY3MZBHmmnOFLi5OzpZBZBKFgyLNKohsrGAcQbZACI9c7Pxh7TkIqyx5jjGAZD';
 
     $response = $facebook->get('/116189751384283/posts?fields=id,message,created_time,full_picture', $accessToken);
     $posts = $response->getGraphEdge();
@@ -34,6 +34,6 @@ class FacebookFeedController extends Controller
         // Do something with the data...
     }
 
-    return view('test', compact('posts'));
+    return view('facebook-feed', compact('posts'));
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
-
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\FacebookFeedController;
-use App\Http\Controllers\PostManagmentController;
-use App\Http\Controllers\SendEmailController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/test', [FacebookFeedController::class, 'scrapeFacebook']);
-
-Route::get('/sendemail', [SendEmailController::class, 'index']);
-Route::post('/sendemail/send', [SendEmailController::class, 'send']);
+Route::get('/', [FacebookFeedController::class, 'scrapeFacebook']);
